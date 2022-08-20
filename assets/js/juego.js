6ebf3dd70211e317bac83be202115adc2d2ad39e
vegadevh <00148816@uca.edu.sv>
2022-08-19 19:03:43 -0600

    Finalizacion de juego, generar nuevo juego y finalizar partida

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -7,9 +7,11 @@ const buttonNuevo = document.querySelector('#buttonNuevo');
 const buttonPedir = document.querySelector('#buttonPedir');
 const buttonDetener = document.querySelector('#buttonDetener');
 
+//Retorna una lista de nodos con todos los elementso que encajen con el parametro enviado
 const puntajes = document.querySelectorAll('small');
 
 const jugadorCartas = document.querySelector('#jugadorCartas');
+const computadoraCartas = document.querySelector('#computadoraCartas');
 
 // const tagImg = document.createElement('img');
 
@@ -65,12 +67,46 @@ const valorCarta = (carta) => {
     return (isNaN(valor)) ? (valor === 'A') ? 11 : 10 : valor * 1;
 }
 
+//Para la logica de la computadora se tomara en cuenta que unicamente debe
+// - Superar el valor del jugador sin pasar de 21 (Si el jugador tiene menos que 21)
+// - Obtener cualquier valor menos a 21 (Si el jugador pierde)
+// - Alcanzar 21 puntos
+const turnoComputadora = (puntosMinimos) => {
+    //Necesario ejecutar por lo menos una vez
+    do {
+        const carta = pedirCartar();
+        const cartaAgregada = document.createElement('img');
+        puntosComputadora = puntosComputadora + valorCarta(carta);
+
+        //Dibuja en el HTML los puntos acumulados en el primer elemento con el tag 'small'
+        puntajes[1].innerText = puntosComputadora;
+        cartaAgregada.classList = 'carta';
+        cartaAgregada.src = `assets/cartas/${carta}.png`;
+        computadoraCartas.append(cartaAgregada);
+    } while ((puntosComputadora < puntosMinimos) && (puntosMinimos <= 21));
+
+    //Determinar el ganador
+    setTimeout(() => {
+        if (puntosComputadora === puntosJugador) {
+            alert('No hay ganador');
+        } else if (puntosJugador > 21) {
+            alert(`Gana la computadora con ${puntosComputadora} VS ${puntosJugador}`);
+        } else if (puntosComputadora > 21) {
+            alert(`GANASTE con ${puntosJugador} VS ${puntosComputadora}`);
+        } else {
+            alert('Computadora gana');
+        }
+    }, 20);
+
+}
+
 //Eventos
 buttonPedir.addEventListener('click', () => {
     const carta = pedirCartar();
     const cartaAgregada = document.createElement('img');
     puntosJugador = puntosJugador + valorCarta(carta);
 
+    //Dibuja en el HTML los puntos acumulados en el primer elemento con el tag 'small'
     puntajes[0].innerText = puntosJugador;
     cartaAgregada.classList = 'carta';
     cartaAgregada.src = `assets/cartas/${carta}.png`;
@@ -78,12 +114,36 @@ buttonPedir.addEventListener('click', () => {
 
     //Logica del juego
     if (puntosJugador > 21) {
+        //disabled: Deshabilitar elemento. 
         buttonPedir.disabled = true;
         console.warn('PASASTE DE 21! PERDISTE!');
+        buttonDetener.disabled = true;
+        turnoComputadora(puntosJugador);
     } else if (puntosJugador === 21) {
         console.warn('GANASTE!');
         buttonPedir.disabled = true;
-    } else {
-
+        buttonDetener.disabled = true;
+        turnoComputadora(puntosJugador);
     }
 })
+
+buttonDetener.addEventListener('click', () => {
+    buttonPedir.disabled = true;
+    buttonDetener.disabled = true;
+    turnoComputadora(puntosJugador);
+});
+
+buttonNuevo.addEventListener('click', () => {
+    buttonDetener.disabled = false;
+    buttonPedir.disabled = false;
+
+    puntosComputadora = 0;
+    puntosJugador = 0;
+    puntajes[0].innerText = puntosJugador;
+    puntajes[1].innerText = puntosComputadora;
+
+    jugadorCartas.innerHTML = '';
+    computadoraCartas.innerHTML = '';
+
+    crearDeck();
+});
